13062085e5d28f42980a422bbbc92e00242cb5c7
Vinay Pal <vinaypal1220@gamil.com>
2024-07-04 23:47:23 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignup';
+
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory category = "mens"/>}/>
+      <Route path='/womens' element={<ShopCategory category = "women"/>}/>
+      <Route path='/kids' element={<ShopCategory category = "kids"/>}/> 
+      <Route path='/product' element={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+
+      </Routes>
+
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
